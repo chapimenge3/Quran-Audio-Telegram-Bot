3498e41a2ea747bcfbb73e3607bdc24a7317b2d2
chapimenge3 <chapimenge3@gmail.com>
2019-12-08 11:33:49 +0300

    by number done

--- main.py
+++ main.py
@@ -60,35 +60,58 @@ Choose below :</strong>"""
         parse_mode=telegram.ParseMode.HTML
     )
     return First
-def bynum(update, context):
+
+
+def bynum1(update, context):
     query = update.callback_query
     bot = context.bot
-    keyboard = []
-    num = 1 
-    for _ in range(12):
-        row =[]
-        for __ in range(10):
-            if _ == 11 and num == 115:
-                break
-            row.append(InlineKeyboardButton(num,callback_data=str(num)))
-            num+=1
-        keyboard.append(row)
-    keyboard.append([InlineKeyboardButton("⬅️  BACk",callback_data="BACK")])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    bot.edit_message_text(
-        chat_id=query.message.chat_id,
-        message_id=query.message.message_id,
-        text="Please Select your choice surah number",
-        reply_markup=reply_markup
-    )
+    buttons = []
+    buttons_row = []
+    x = 0
+    for number in range(1, 58):
+        buttons_row.append(InlineKeyboardButton(number, callback_data=str(number)))
+        x += 1
+        if x == 6:
+            buttons.append(buttons_row)
+            buttons_row = []
+            x = 0
+    if buttons_row:
+        buttons.append(buttons_row)
+    back_button = InlineKeyboardButton("cancel", callback_data="cancel")
+    next_button = InlineKeyboardButton("next", callback_data="next")
+    menu_buttons = [back_button, next_button]
+    buttons.append(menu_buttons)
+    query.edit_message_text("Numbers", reply_markup=InlineKeyboardMarkup(buttons) )
     return Second
+
+
+def bynum2(update,context):
+    query = update.callback_query
+    buttons = []
+    buttons_row = []
+    x = 0
+    for number in range(57, 115):
+        buttons_row.append(InlineKeyboardButton(number, callback_data=str(number)))
+        x += 1
+        if x == 6:
+            buttons.append(buttons_row)
+            buttons_row = []
+            x = 0
+    if buttons_row:
+        buttons.append(buttons_row)
+    back_button = InlineKeyboardButton("back", callback_data="back")
+    next_button = InlineKeyboardButton("cancel", callback_data="cancel")
+    menu_buttons = [back_button, next_button]
+    buttons.append(menu_buttons)
+    query.edit_message_text("Numbers", reply_markup=InlineKeyboardMarkup(buttons))
+    return Second
+
 def byname(update,context):
     query = update.callback_query
     bot = context.bot
     keyboard = []
     num = 0
     l=[]
-    print(surah,"\n\n")
     r = list(surah.items()) 
     for _ in range(25):
         row =[]
@@ -101,8 +124,7 @@ def byname(update,context):
             num+=1
             print(num)
         keyboard.append(row)
-    # keyboard.append(InlineKeyboardButton("chapi menge",callback_data=str(num)))
-    keyboard.append([InlineKeyboardButton("⬅️  BACk",callback_data="BACK")])
+    keyboard.append([InlineKeyboardButton("⬅️  BACk",callback_data="back")])
     reply_markup = InlineKeyboardMarkup(keyboard)
     bot.edit_message_text(
         chat_id=query.message.chat_id,
@@ -111,21 +133,18 @@ def byname(update,context):
         reply_markup=reply_markup
     )
     return Second
+
 def file(update,context):
     query = update.callback_query
-    file_id = data[str(query.data)]
+    file_id = data[surah[str(query.data)]]
     bot = context.bot 
-    for i in surah:
-        print(i)
     bot.edit_message_text(
         chat_id=query.message.chat_id,
         message_id=query.message.message_id,
-        text="""To get started again send me /start!
+        text="""To get started again send me /start
 As-salāmu ʿalaykum wa-raḥmatu llāhi wa-barakātuhu."""
     )
-    BOT.sendAudio(chat_id=query.message.chat_id ,
-                   audio=file_id
-                   )
+    BOT.sendAudio(chat_id=query.message.chat_id , audio=file_id)
     return First
 def save(update,context):
     global Num
@@ -144,13 +163,15 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)] ,
         states={ 
-            First : [CallbackQueryHandler(bynum,pattern='^' + str(ONE) + '$'),
+            First : [CallbackQueryHandler(bynum1,pattern='^' + str(ONE) + '$'),
                      CallbackQueryHandler(byname,pattern='^' + str(TWO) + '$'),
                      MessageHandler(Filters.audio, save),
                      MessageHandler(Filters.document,save)],
             Second : [CallbackQueryHandler(file,pattern=r"\d|\d\d|\d\d\d"),
-                      CallbackQueryHandler(startover,pattern="BACK"),
+                      CallbackQueryHandler(startover,pattern="back"),
+                      CallbackQueryHandler(bynum2,pattern="next"),
                     ],
+            THREE : []
         },
         fallbacks=[CommandHandler('start', start)] ,
         # per_message=True
@@ -161,4 +182,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+<<<<<<< HEAD
 fil.close()
+=======
+>>>>>>> by number done
