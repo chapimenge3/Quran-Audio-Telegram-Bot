6b9e333965b510e659c349fe61bf127e4ed20901
Chapi Menge <chapimenge3@gmail.com>
2023-03-25 22:27:39 +0300

    add: update on downloading audio logic

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from deta import Deta
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
-
+import httpx
 
 TOKEN = os.environ['TOKEN']
 
@@ -15,6 +15,8 @@ WELCOME_MESSAGE = """Aselamaleykum {name}
 Welcome to the Quran Bot you can use this bot to listen to the recitation of the Quran with different reciters.
 
 Please click on the button below to choose the reciter you want to listen to.
+
+Join my channel https://t.me/codewizme to learn more about me and my projects.
 """
 
 
@@ -33,391 +35,23 @@ class TelegramWebhook(BaseModel):
     poll_answer: Optional[dict]
 
 
-
 app = FastAPI()
 deta = Deta(os.environ['DETA_PROJECT_KEY'])
 db = deta.Base('file_id')
-user_db = deta.Base('user')
+user_db = deta.Base('quran_bot_user')
 surah_db = deta.Base('surah')
+reciters_db = deta.Base('reciters')
 
+RECITATIONS = []
+fetch_reciters = reciters_db.fetch()
+while True:
+    for i in fetch_reciters.items:
+        RECITATIONS.append(i)
+    if fetch_reciters.last is None:
+        break
+    fetch_reciters = reciters_db.fetch(last=fetch_reciters.last)
 
-RECITATIONS = [
-    {
-        "name": "Abdullah Awad al-Juhani",
-        "id": 1,
-        "download_link": "https://download.quranicaudio.com/quran/abdullaah_3awwaad_al-juhaynee"
-    },
-    {
-        "name": "Abdullah Basfar",
-        "id": 2,
-        "download_link": "https://download.quranicaudio.com/quran/abdullaah_basfar"
-    },
-    {
-        "name": "Abdur-Rahman as-Sudais",
-        "id": 7,
-        "download_link": "https://download.quranicaudio.com/quran/abdurrahmaan_as-sudays"
-    },
-    {
-        "name": "Ali Abdur-Rahman al-Huthaify",
-        "id": 8,
-        "download_link": "https://download.quranicaudio.com/quran/huthayfi"
-    },
-    {
-        "name": "AbdulMuhsin al-Qasim",
-        "id": 11,
-        "download_link": "https://download.quranicaudio.com/quran/abdul_muhsin_alqasim"
-    },
-    {
-        "name": "AbdulBari ath-Thubaity",
-        "id": 15,
-        "download_link": "https://download.quranicaudio.com/quran/thubaity"
-    },
-    {
-        "name": "Ahmed ibn Ali al-Ajmy",
-        "id": 19,
-        "download_link": "https://download.quranicaudio.com/quran/ahmed_ibn_3ali_al-3ajamy"
-    },
-    {
-        "name": "AbdulAzeez al-Ahmad",
-        "id": 21,
-        "download_link": "https://download.quranicaudio.com/quran/abdulazeez_al-ahmad"
-    },
-    {
-        "name": "AbdulBaset AbdulSamad [Murattal]",
-        "id": 37,
-        "download_link": "https://download.quranicaudio.com/quran/abdul_basit_murattal"
-    },
-    {
-        "name": "AbdulWadud Haneef",
-        "id": 40,
-        "download_link": "https://download.quranicaudio.com/quran/abdulwadood_haneef"
-    },
-    {
-        "name": "Aziz Alili",
-        "id": 44,
-        "download_link": "https://download.quranicaudio.com/quran/aziz_alili"
-    },
-    {
-        "name": "AbdulBaset AbdulSamad [Mujawwad]",
-        "id": 50,
-        "download_link": "https://download.quranicaudio.com/quran/abdulbaset_mujawwad"
-    },
-    {
-        "name": "Al-Hussayni Al-'Azazy (with Children)",
-        "id": 55,
-        "download_link": "https://download.quranicaudio.com/quran/alhusaynee_al3azazee_with_children"
-    },
-    {
-        "name": "Abdur-Razaq bin Abtan al-Dulaimi [Mujawwad]",
-        "id": 68,
-        "download_link": "https://download.quranicaudio.com/quran/abdulrazaq_bin_abtan_al_dulaimi"
-    },
-    {
-        "name": "Abdullah Khayat",
-        "id": 72,
-        "download_link": "https://download.quranicaudio.com/quran/khayat"
-    },
-    {
-        "name": "Adel Kalbani",
-        "id": 81,
-        "download_link": "https://download.quranicaudio.com/quran/adel_kalbani"
-    },
-    {
-        "name": "AbdulKareem Al Hazmi",
-        "id": 106,
-        "download_link": "https://download.quranicaudio.com/quran/abdulkareem_al_hazmi"
-    },
-    {
-        "name": "Abdul-Mun'im Abdul-Mubdi'",
-        "id": 108,
-        "download_link": "https://download.quranicaudio.com/quran/abdulmun3im_abdulmubdi2"
-    },
-    {
-        "name": "Abdur-Rashid Sufi",
-        "id": 109,
-        "download_link": "https://download.quranicaudio.com/quran/abdurrashid_sufi"
-    },
-    {
-        "name": "Ahmad al-Huthaify",
-        "id": 113,
-        "download_link": "https://download.quranicaudio.com/quran/ahmad_alhuthayfi"
-    },
-    {
-        "name": "Abu Bakr al-Shatri [Taraweeh]",
-        "id": 115,
-        "download_link": "https://download.quranicaudio.com/quran/abu_bakr_ash-shatri_tarawee7"
-    },
-    {
-        "name": "Abdullah Matroud",
-        "id": 124,
-        "download_link": "https://download.quranicaudio.com/quran/abdullah_matroud"
-    },
-    {
-        "name": "AbdulWadood Haneef",
-        "id": 125,
-        "download_link": "https://download.quranicaudio.com/quran/abdul_wadood_haneef_rare"
-    },
-    {
-        "name": "Ahmad Nauina",
-        "id": 126,
-        "download_link": "https://download.quranicaudio.com/quran/ahmad_nauina"
-    },
-    {
-        "name": "Akram Al-Alaqmi",
-        "id": 127,
-        "download_link": "https://download.quranicaudio.com/quran/akram_al_alaqmi"
-    },
-    {
-        "name": "Ali Hajjaj Alsouasi",
-        "id": 128,
-        "download_link": "https://download.quranicaudio.com/quran/ali_hajjaj_alsouasi"
-    },
-    {
-        "name": "Asim Abdul Aleem",
-        "id": 135,
-        "download_link": "https://download.quranicaudio.com/quran/asim_abdulaleem"
-    },
-    {
-        "name": "Abdallah Abdal",
-        "id": 136,
-        "download_link": "https://download.quranicaudio.com/quran/abdallah_abdal"
-    },
-    {
-        "name": "Abdullah Ali Jabir",
-        "id": 158,
-        "download_link": "https://download.quranicaudio.com/quran/ali_jaber"
-    },
-    {
-        "name": "Bandar Baleela",
-        "id": 160,
-        "download_link": "https://download.quranicaudio.com/quran/bandar_baleelacomplete"
-    },
-    {
-        "name": "Dr. Shawqy Hamed [Murattal]",
-        "id": 74,
-        "download_link": "https://download.quranicaudio.com/quran/dr.shawqy_7amedmurattal"
-    },
-    {
-        "name": "Fares Abbad",
-        "id": 14,
-        "download_link": "https://download.quranicaudio.com/quran/fares"
-    },
-    {
-        "name": "Fatih Seferagic",
-        "id": 134,
-        "download_link": "https://download.quranicaudio.com/quran/fatih_seferagic"
-    },
-    {
-        "name": "Hani ar-Rifai",
-        "id": 27,
-        "download_link": "https://download.quranicaudio.com/quran/rifai"
-    },
-    {
-        "name": "Hamad Sinan",
-        "id": 64,
-        "download_link": "https://download.quranicaudio.com/quran/hamad_sinan"
-    },
-    {
-        "name": "Hatem Farid",
-        "id": 85,
-        "download_link": "https://download.quranicaudio.com/quran/hatem_faridcollection"
-    },
-    {
-        "name": "Ibrahim Al-Jibrin",
-        "id": 28,
-        "download_link": "https://download.quranicaudio.com/quran/jibreen"
-    },
-    {
-        "name": "Imad Zuhair Hafez",
-        "id": 93,
-        "download_link": "https://download.quranicaudio.com/quran/imad_zuhair_hafez"
-    },
-    {
-        "name": "Ibrahim Al Akhdar",
-        "id": 103,
-        "download_link": "https://download.quranicaudio.com/quran/ibrahim_al_akhdar"
-    },
-    {
-        "name": "Idrees Abkar",
-        "id": 116,
-        "download_link": "https://download.quranicaudio.com/quran/idrees_akbar"
-    },
-    {
-        "name": "Khalid al-Qahtani",
-        "id": 9,
-        "download_link": "https://download.quranicaudio.com/quran/khaalid_al-qahtaanee"
-    },
-    {
-        "name": "Khalid Al Ghamdi",
-        "id": 105,
-        "download_link": "https://download.quranicaudio.com/quran/khalid_alghamdi"
-    },
-    {
-        "name": "Khalifah Taniji",
-        "id": 161,
-        "download_link": "https://download.quranicaudio.com/quran/khalifah_taniji"
-    },
-    {
-        "name": "Mishari Rashid al-`Afasy",
-        "id": 5,
-        "download_link": "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee"
-    },
-    {
-        "name": "Muhammad Siddiq al-Minshawi",
-        "id": 6,
-        "download_link": "https://download.quranicaudio.com/quran/muhammad_siddeeq_al-minshaawee"
-    },
-    {
-        "name": "Muhammad Jibreel",
-        "id": 12,
-        "download_link": "https://download.quranicaudio.com/quran/muhammad_jibreelcomplete"
-    },
-    {
-        "name": "Muhammad al-Mehysni",
-        "id": 26,
-        "download_link": "https://download.quranicaudio.com/quran/mehysni"
-    },
-    {
-        "name": "Muhammad al-Luhaidan",
-        "id": 53,
-        "download_link": "https://download.quranicaudio.com/quran/muhammad_alhaidan"
-    },
-    {
-        "name": "Muhammad Abdul-Kareem",
-        "id": 70,
-        "download_link": "https://download.quranicaudio.com/quran/muhammad_abdulkareem"
-    },
-    {
-        "name": "Mustafa al-`Azawi",
-        "id": 71,
-        "download_link": "https://download.quranicaudio.com/quran/mustafa_al3azzawi"
-    },
-    {
-        "name": "Muhammad Hassan",
-        "id": 79,
-        "download_link": "https://download.quranicaudio.com/quran/mu7ammad_7assan"
-    },
-    {
-        "name": "Mostafa Ismaeel",
-        "id": 88,
-        "download_link": "https://download.quranicaudio.com/quran/mostafa_ismaeel"
-    },
-    {
-        "name": "Muhammad Sulaiman Patel",
-        "id": 90,
-        "download_link": "https://download.quranicaudio.com/quran/muhammad_patel"
-    },
-    {
-        "name": "Mohammad Al-Tablawi",
-        "id": 91,
-        "download_link": "https://download.quranicaudio.com/quran/mohammad_altablawi"
-    },
-    {
-        "name": "Mohammad Ismaeel Al-Muqaddim",
-        "id": 92,
-        "download_link": "https://download.quranicaudio.com/quran/mohammad_ismaeel_almuqaddim"
-    },
-    {
-        "name": "Muhammad Ayyoob [Taraweeh]",
-        "id": 107,
-        "download_link": "https://download.quranicaudio.com/quran/muhammad_ayyoob_hq"
-    },
-    {
-        "name": "Masjid Quba Taraweeh 1434",
-        "id": 118,
-        "download_link": "https://download.quranicaudio.com/quran/masjid_quba_1434"
-    },
-    {
-        "name": "Muhammad Khaleel",
-        "id": 119,
-        "download_link": "https://download.quranicaudio.com/quran/muhammad_khaleel"
-    },
-    {
-        "name": "Mahmoud Khaleel Al-Husary",
-        "id": 122,
-        "download_link": "https://download.quranicaudio.com/quran/mahmood_khaleel_al-husaree_iza3a"
-    },
-    {
-        "name": "Mahmood Ali Al-Bana",
-        "id": 129,
-        "download_link": "https://download.quranicaudio.com/quran/mahmood_ali_albana"
-    },
-    {
-        "name": "Maher al-Muaiqly",
-        "id": 159,
-        "download_link": "https://download.quranicaudio.com/quran/maher_almu3aiqlyyear1440"
-    },
-    {
-        "name": "Nabil ar-Rifai",
-        "id": 10,
-        "download_link": "https://download.quranicaudio.com/quran/nabil_rifa3i"
-    },
-    {
-        "name": "Nasser Al Qatami",
-        "id": 104,
-        "download_link": "https://download.quranicaudio.com/quran/nasser_bin_ali_alqatami"
-    },
-    {
-        "name": "Sa`ud ash-Shuraym",
-        "id": 4,
-        "download_link": "https://download.quranicaudio.com/quran/sa3ood_al-shuraym"
-    },
-    {
-        "name": "Saad al-Ghamdi",
-        "id": 13,
-        "download_link": "https://download.quranicaudio.com/quran/sa3d_al-ghaamidicomplete"
-    },
-    {
-        "name": "Sahl Yasin",
-        "id": 17,
-        "download_link": "https://download.quranicaudio.com/quran/sahl_yaaseen"
-    },
-    {
-        "name": "Salah Bukhatir",
-        "id": 18,
-        "download_link": "https://download.quranicaudio.com/quran/salaah_bukhaatir"
-    },
-    {
-        "name": "Sudais and Shuraym",
-        "id": 20,
-        "download_link": "https://download.quranicaudio.com/quran/sodais_and_shuraim"
-    },
-    {
-        "name": "Saleh al Taleb",
-        "id": 35,
-        "download_link": "https://download.quranicaudio.com/quran/saleh_al_taleb"
-    },
-    {
-        "name": "Salah al-Budair",
-        "id": 43,
-        "download_link": "https://download.quranicaudio.com/quran/salahbudair"
-    },
-    {
-        "name": "Sadaqat `Ali",
-        "id": 61,
-        "download_link": "https://download.quranicaudio.com/quran/sadaqat_ali"
-    },
-    {
-        "name": "Salah Al-Hashim",
-        "id": 80,
-        "download_link": "https://download.quranicaudio.com/quran/salah_alhashim"
-    },
-    {
-        "name": "Tawfeeq ibn Sa`id as-Sawa'igh",
-        "id": 23,
-        "download_link": "https://download.quranicaudio.com/quran/tawfeeq_bin_saeed-as-sawaaigh"
-    },
-    {
-        "name": "Wadee Hammadi Al Yamani",
-        "id": 130,
-        "download_link": "https://download.quranicaudio.com/quran/wadee_hammadi_al-yamani"
-    },
-    {
-        "name": "Yasser ad-Dussary",
-        "id": 97,
-        "download_link": "https://download.quranicaudio.com/quran/yasser_ad-dussary"
-    }
-]
+RECITATIONS.sort(key=lambda x: x['name'])
 
 fetch_surah = surah_db.fetch()
 all_surah = {}
@@ -428,11 +62,27 @@ while True:
         break
     fetch_surah = surah_db.fetch(last=fetch_surah.last)
 
+
+def download_audio(url, path):
+    try:
+        with httpx.stream('GET', url) as r:
+            with open(path, 'wb') as f:
+                for chunk in r.iter_bytes():
+                    f.write(chunk)
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
 def start(update: Update, context: CallbackContext):
     user = update.effective_user or update.effective_chat
     name = getattr(user, "first_name", '')
     keyboard = [
-        [InlineKeyboardButton("Start", callback_data='start')]
+        [InlineKeyboardButton("Start", callback_data='start')],
+        [InlineKeyboardButton(
+            "Support", url='https://www.buymeacoffee.com/chapimenge')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(WELCOME_MESSAGE.format(
@@ -440,7 +90,6 @@ def start(update: Update, context: CallbackContext):
 
     user_dict = user.to_dict()
     user_dict['key'] = str(user.id)
-    print(user_dict)
     user_db.put(user_dict)
 
 
@@ -503,7 +152,6 @@ def show_suras(update: Update, context: CallbackContext):
     keyboard.append(util_btn)
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(text="Choose a sura", reply_markup=reply_markup)
-    
 
 
 def send_audio(update: Update, context: CallbackContext):
@@ -513,11 +161,46 @@ def send_audio(update: Update, context: CallbackContext):
     reciter_id = int(surah_id.split(':')[1])
     surah_id = surah_id.split(':')[2]
     surah_id = surah_id.zfill(3)
-    file_id = db.get(f'{surah_id}-{reciter_id}')
     query.edit_message_text(text='Sending audio...')
-    context.bot.send_audio(chat_id=query.message.chat_id,
-                           audio=file_id['file_id'])
-    
+    file_id = db.get(f'{surah_id}-{reciter_id}')
+    if not file_id:
+        path = f'{surah_id}-{reciter_id}.mp3'
+        download_url = None
+        for reciter in RECITATIONS:
+            if reciter['key'] == str(reciter_id):
+                download_url = reciter['download_link']
+                break
+
+        if not download_url:
+            query.edit_message_text(
+                text='Something went wrong, Please try again later.')
+            return
+        query.edit_message_text(
+            text='Downloading audio, Please wait...')
+        download_url = f'{download_url}/{surah_id}.mp3'
+        download_audio(download_url, path)
+        if not download_url or not os.path.exists(path):
+            query.edit_message_text(
+                text='Something went wrong, Please try again later.')
+            return
+
+        query.edit_message_text(
+            text='Uploading audio, Please wait...')
+        file_id = context.bot.send_audio(chat_id=query.message.chat_id,
+                                         audio=open(path, 'rb'))
+        os.remove(path)
+        db.put({
+            "key": f'{surah_id}-{reciter_id}',
+            "file_id": file_id['audio']['file_id'],
+            "name": all_surah[surah_id],
+            "surah_number": surah_id,
+            "reciter": reciter_id
+        })
+        query.delete_message()
+    else:
+        context.bot.send_audio(chat_id=query.message.chat_id,
+                               audio=file_id['file_id'])
+
     now = datetime.now()
     today = now.strftime("%d/%m/%Y %H:%M:%S")
     stat = user_db.get(today)
@@ -564,6 +247,7 @@ def webhook(data: TelegramWebhook):
     dispatcher.process_update(update)
 
     return {"status": "ok"}
+
 
 @app.get("/")
 def index():
